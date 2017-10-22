d7415cfccbcc8f28d766324682c992eebe394d32
Ramesh <gurams@gmail.com>
2017-10-22 04:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   const arr=[]
 
   for (let i=0 ; musicians.length>i; i++){
-    arr.push(${musicians[i]}+" plays "+${instruments[i]});
+    arr.push(''${musicians[i]} plays ${instruments[i]}'');
   }
 return arr;
 }
